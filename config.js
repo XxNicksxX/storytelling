7d13c2b6d853234fda65a1190e191629dcf7632c
Nicolas lopes <93435464+XxNicksxX@users.noreply.github.com>
2023-03-01 21:51:02 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDUV_EbSN3aLmUlVd5KTTNTR_9V9WCTBBk",
+  authDomain: "historia-c1ee1.firebaseapp.com",
+  databaseURL: "https://historia-c1ee1-default-rtdb.firebaseio.com",
+  projectId: "historia-c1ee1",
+  storageBucket: "historia-c1ee1.appspot.com",
+  messagingSenderId: "5355262091",
+  appId: "1:5355262091:web:9cca2c6feedf39e8fe048e",
+};
